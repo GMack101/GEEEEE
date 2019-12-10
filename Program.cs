7f75f28c73f5b6c782fab9e6c5493deeb2a6008b
Gee <tuca101@cs.com>
2019-12-10 15:08:10 -0600

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GEEEEE
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
